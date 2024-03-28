788c7177adcf04b91b6e5b8212571250d3e93276
rushikeshtinkhede <162783112+rushikeshtinkhede@users.noreply.github.com>
2024-03-28 18:35:13 +0300

    Add files via upload

---
+++ Weather Website/index.js
@@ -0,0 +1,57 @@
+const apiKey = "d5309c7fe60a5fb1ab08c239a2fa1c17";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherimage = document.querySelector(".weather-img");
+const errorDisplay = document.querySelector(".error");
+
+function checkWeather(city) {
+    fetch(`${apiUrl}${city}&appid=${apiKey}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("City not found");
+            }
+            return response.json();
+        })
+        .then(data => {
+            weatherUpdate(data);
+        })
+        .catch(error => {
+            displayError();
+        });
+}
+
+function weatherUpdate(data) {
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}<sup>°</sup>C`;
+    document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
+    document.querySelector(".wind").innerHTML = `${data.wind.speed} km/h`;
+    weatherImg(data.weather[0].main);
+    hideError();
+}
+
+function weatherImg(weatherType) {
+    const weatherImgs = {
+        "Clouds": "clouds.png",
+        "Clear": "clear.png",
+        "Rain": "rain.png",
+        "Drizzle": "drizzle.png",
+        "Mist": "mist.png",
+        "Snow": "snow.png"
+    };
+    weatherimage.src = weatherImgs[weatherType] || "clear.png";
+}
+
+function displayError() {
+    errorDisplay.style.display = "block";
+}
+
+function hideError() {
+    errorDisplay.style.display = "none";
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
